8622fe465f20acf694b72c4f7a307ae5b50591ea
ngocho <ngocho@umich.edu>
2023-09-28 20:06:07 -0400

    area

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -30,10 +30,10 @@ class Rectangle():
     #
     # It first verifies inputs and return "Invalid input" if they are invalid.
     # Otherwise, it returns the area of the rectangle.
-
-    # YOUR CODE HERE
-
-
+    def area(self):
+        if not self.verify_input():
+            return "Invalid input"
+        return str(self.width * self.height)
 
     # Create the "perimeter" method
     #
